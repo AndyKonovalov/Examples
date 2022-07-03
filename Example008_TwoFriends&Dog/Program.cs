62d4ec5658a0db832344d55228f1d93007e9da5a
Andrei <fmwar92@gmail.com>
2022-07-03 17:12:27 +0300

    Заменил подсчеты скорости, времени и дистанции методами, добавил экстраполяцию, и округлил дистанцию

--- Example008_TwoFriends&Dog/Program.cs
+++ Example008_TwoFriends&Dog/Program.cs
@@ -7,31 +7,42 @@ int meetDistance = 10;
 int count = 0; // Количество раз, которое собака пробежит от одного друга к другому
 double time = 0;
 
+double DistFriend1()
+{
+    time = distance / (firstFriendSpeed + dogSpeed); // Время, за которое собака добежит до первого друга
+    friend = 2;
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    return distance; 
+}
+
+double DistFriend2()
+{
+    time = distance / (secondFriendSpeed + dogSpeed); // Время, за которое собака добежит до второго друга
+    friend = 1;
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    return distance;
+}
+
 while (distance > meetDistance)
 {
     if (friend == 1)
 {
-
-    time = distance / (firstFriendSpeed + dogSpeed); // Время, за которое собака добежит до первого друга
-    friend = 2;
-    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-    Console.Write ("Текущее расстояние между друзьями ");
-    Console.WriteLine (distance);
+    // time = distance / (firstFriendSpeed + dogSpeed); // Время, за которое собака добежит до первого друга
+    // friend = 2;
+    // distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    Console.WriteLine ($"Текущее расстояние между друзьями {Math.Round(DistFriend1(), 2)}");
 }
    
     else 
     
     {
     
-    time = distance / (secondFriendSpeed + dogSpeed); // Время, за которое собака добежит до второго друга
-    friend = 1;
-    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-    Console.Write ("Текущее расстояние между друзьями ");
-    Console.WriteLine (distance);
+    // time = distance / (secondFriendSpeed + dogSpeed); // Время, за которое собака добежит до второго друга
+    // friend = 1;
+    // distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    Console.WriteLine ($"Текущее расстояние между друзьями {Math.Round(DistFriend2(), 2)}");
     }
 
     count++;
 }
-Console.Write ("Собака пробежит ");
-Console.Write (count);
-Console.Write (" раз(а)");
+Console.Write ($"Собака пробежит между друзьями {count} раз(а)");
